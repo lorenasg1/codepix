b6c230b5a505daf83d5316966467b52fd51dc3fc
lorenasg1 <lorenasg1@gmail.com>
2021-02-05 13:32:20 -0300

    fix: imports

--- application/kafka/process.go
+++ application/kafka/process.go
@@ -28,7 +28,7 @@ func NewKafkaProcessor(database *gorm.DB, producer *ckafka.Producer, deliveryCha
 
 func (k *KafkaProcessor) Consume() {
 	configMap := &ckafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+		"rocessor.servers":  os.Getenv("KAFKA_rocessor_SERVERS"),
 		"group.id":          os.Getenv("KAFKA_CONSUMER_GROUP_ID"),
 		"auto.offset.reset": "earliest",
 	}
