ea369fc65beb494f3fe5d72362cd1f4f3589543c
lorenasg1 <lorenasg1@gmail.com>
2021-02-05 10:37:24 -0300

    Terceiro dia

--- cmd/all.go
+++ cmd/all.go
@@ -31,21 +31,22 @@ var gRPCPortNumber int
 var allCmd = &cobra.Command{
 	Use:   "all",
 	Short: "Run gRPC and a Kafka Consumer",
+
 	Run: func(cmd *cobra.Command, args []string) {
 		database := db.ConnectDB(os.Getenv("env"))
-		go grpc.StartGrpcServer(database, gRPCPortNumber)
+		go grpc.StartGrpcServer(database, portNumber)
 
 		deliveryChan := make(chan ckafka.Event)
 		producer := kafka.NewKafkaProducer()
 		go kafka.DeliveryReport(deliveryChan)
-		KafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
-		KafkaProcessor.Consume()
+		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
+		kafkaProcessor.Consume()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(allCmd)
-	allCmd.Flags().IntVarP(&gRPCPortNumber, "grpc-port", "p", 50051, "gRPC Port")
+	allCmd.Flags().IntVarP(&gRPCPortNumber, "grpc-port", "p", 500051, "gRPC Port")
 
 	// Here you will define your flags and configuration settings.
 
